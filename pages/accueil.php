9e183abe300773f785b6d253a7b5cad581ec37dc
armel-pltr <armelpltr14@gmail.com>
2024-12-20 16:04:27 +0100

    modif

--- pages/accueil.php
+++ pages/accueil.php
@@ -4,11 +4,13 @@ $data=yaml_parse_file('data/accueil.yaml');
 
 echo "<div class='container'>";
 echo "<div class='content-wrapper'>";
-echo "<img src='../images/".$data["photo"]."' class='imagebvn' />";
+echo "<img src='../images/".$data["photo1"]."' class='imagebvn' />";
+echo "</div>";
 echo "<p>".$data["prenom"]." ".$data["nom"]."</p>";
 echo "<p>".$data["accroche"]."</p>";
 echo "<p>".$data["presentation"]."</p>";
-echo "</div>";
+echo "<img src='../images/".$data["photo2"]."' class='imgmoi' />";
+
 echo "</div>";
 
 
