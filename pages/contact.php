9383bda306f93e40227be585bdbbeda1f347fb5d
unknown <armelpltr14@gmail.com>
2024-12-21 15:45:15 +0100

    contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,25 +1,36 @@
 <?php
-$data=yaml_parse_file('data/contact.yaml');
+$data = yaml_parse_file('data/contact.yaml');
 
-echo "<div class='wrapper'>";  
-
-echo "<div class='container-contact'>"; 
-
-foreach($data AS $contact){
-
-echo "<h1>Contact</h1>";  
+$recaptcha_site_key = '6Le3XJIqAAAAAGiQ35ZJnjrL1LS08TgQmx25PP9';
+$recaptcha_secret_key = '6Le3XJIqAAAAANjWl8FPXX86NX-PRlB9sfNVC2-8';
 
 
-echo "<h2>".$contact["nom"]."</h2>";
-echo "<p>".$contact["adresse"]."</p>";
-echo "<p>".$contact["objet"]."</p>";
-echo "<p>".$contact["contenu-message"]."</p>";
-echo "<p>".$contact["captcha"]."</p>";
+
+echo "<div class='wrapper'>";
+echo "<div class='container-contact'>";
+echo "<h1>Contact</h1>";
+
+echo "<form method='POST' action=''>";
+
+echo "<label for='nom'>Nom :</label>";
+echo "<input type='text' id='nom' name='nom' placeholder='" . $data[0]['nom'] . "' required>";
+
+echo "<label for='adresse'>Adresse e-mail :</label>";
+echo "<input type='email' id='adresse' name='adresse' placeholder='" . $data[0]['adresse'] . "' required>";
+
+echo "<label for='objet'>Objet :</label>";
+echo "<input type='text' id='objet' name='objet' placeholder='" . $data[0]['objet'] . "' required>";
+
+echo "<label for='contenu-message'>Message :</label>";
+echo "<textarea id='contenu-message' name='contenu-message' placeholder='" . $data[0]['contenu-message'] . "' required></textarea>";
+
+echo "<div class='g-recaptcha' data-sitekey='" . $recaptcha_site_key . "'></div>";
+
+echo "<button type='submit'>Envoyer</button>";
+echo "</form>";
 
 echo "</div>";
 echo "</div>";
-
-}
-
-
 ?>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
